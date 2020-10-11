1082454ad6b1172d59e323125cc791f8782d5e41
Bioman1464 <1235jk106m3A>
2020-10-11 18:04:11 +0300

    add: application class, dagger component for app

---
+++ app/src/main/java/com/example/newsapp/AppComponent.kt
@@ -0,0 +1,25 @@
+package com.example.newsapp
+
+import android.app.Application
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.AndroidInjectionModule
+import dagger.android.AndroidInjector
+import javax.inject.Singleton
+
+
+@Singleton
+@Component (modules = [AndroidInjectionModule::class])
+interface AppComponent {
+
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        fun application(application: Application): Builder
+
+        fun build(): AppComponent
+    }
+
+    fun inject (myApplication: MyApplication)
+
+}
---
+++ app/src/main/java/com/example/newsapp/MyApplication.kt
@@ -0,0 +1,18 @@
+package com.example.newsapp
+
+import android.app.Activity
+import android.app.Application
+import android.app.Service
+import androidx.fragment.app.Fragment
+import dagger.android.AndroidInjector
+import dagger.android.DaggerApplication
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasAndroidInjector
+import javax.inject.Inject
+
+class MyApplication: Application(), HasAndroidInjector {
+
+    @Inject lateinit var androidInjector : DispatchingAndroidInjector<Any>
+
+    override fun androidInjector(): AndroidInjector<Any> = androidInjector
+}
